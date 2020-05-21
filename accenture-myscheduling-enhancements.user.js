03c9614d9f2fa8eb749f9565eca7ecf83d13a7b1
Marcin Wisnicki <mwisnicki@gmail.com>
2020-05-21 00:33:35 -0400

    Update accenture-myscheduling-enhancements.user.js

--- accenture-myscheduling-enhancements.user.js
+++ accenture-myscheduling-enhancements.user.js
@@ -5,27 +5,85 @@
 // @match       https://mysched.accenture.com/me
 // @match       https://mysched.accenture.com/me/*
 // @grant       GM.addStyle
-// @version     1.0
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @version     2.0
 // @author      mwisnicki@gmail.com
 // ==/UserScript==
 
 'use strict';
 
+// Configuration
+// =============
+
+const debug = GM_getValue('debug', false);
+// persisting data makes it available regardless of whether you visited main page but at the risk of showing stale data
+const persistent = GM_getValue('persistent', true);
+
+GM_registerMenuCommand((debug ? 'Disable' : 'Enable') + ' debug', () => {
+  GM_setValue('debug', !debug);
+  location.reload();
+});
+
+GM_registerMenuCommand((debug ? 'Disable' : 'Enable') + ' persistence', () => {
+  GM_setValue('persistent', !persistent);
+  location.reload();
+});
+
+
+// Storage
+// =======
+
+const sources = persistent && GM_getValue('sources', {}) || {};
+const roles = persistent && GM_getValue('roles', {}) || {};
+
+
+// Styles
+// ======
+
 GM.addStyle(`
 
-.userscript {
+.GM_injected.endDate {
     color: #0275d8;
     font-size: 0.8em;
 }
 
+.GM_injected.status {
+    cursor: default;
+}
+
+.GM_ht:hover .GM_tooltip {
+    display: block;
+}
+
+.GM_tooltip {
+    display: none;
+    color: red;
+    margin-left: 10px;
+    margin-top: -4px;
+    max-width: 700px;
+    position: absolute;
+    z-index: 1000;
+    background: wheat;
+    font: initial;
+    font-size: small;
+    box-shadow: 2px 2px 5px 3px #0000005e;
+}
+
 `);
 
-const sources = {};
-const roles = {};
+
+// Helpers
+// =======
 
 function getElementByXPath(path, parent) {
     return document.evaluate(path, parent || document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
+
+
+// Logic
+// =====
 
 function updateRow(roleRow) {
     const idElement = roleRow.querySelector('.role-id');
@@ -34,22 +92,41 @@ function updateRow(roleRow) {
 
     const id = idElement.innerText;
     const durationCell = getElementByXPath(".//span[contains(text(),'Duration')]/..", roleRow);
+    const favoriteCell = getElementByXPath(".//favorite-icon/..", roleRow);
     const source = sources[id];
-
-    if (!source) {
-        console.error("Missing source for %s", id);
-        return;
+    const role = roles[id];
+  
+    if (debug) {
+      roleRow.GM_data = { source, role };
+      idElement.classList.add("GM_ht")
+      idElement.insertAdjacentHTML('beforeend', `<div class="GM_injected GM_tooltip"><pre>${JSON.stringify({ role, source }, undefined, 4)}</pre></div>`);
     }
+  
+    if (source && durationCell) {
+        const endDate = new Date(source.roleEndDate);
+        const endDateStr = endDate.toISOString().split('T')[0];
 
-    const endDate = new Date(source.roleEndDate);
-    const endDateStr = endDate.toISOString().split('T')[0];
-
-    durationCell.insertAdjacentHTML('beforeend', `<div class="userscript endDate">${endDateStr}</div>`);
+        durationCell.insertAdjacentHTML('beforeend', `<div class="GM_injected endDate">${endDateStr}</div>`);
+    } else {
+        console.error("Missing source for %s", id);
+    }
+  
+    // TODO this only works if user visits main page first or persistence is enabled, otherwise TrackAssignment is not loaded
+    if (role && favoriteCell) {
+      let statusStr = "";
+      if (role.AppliedDate) {
+        statusStr += `<span title="Applied ${role.AppliedDate}">✉</span>`;
+      }
+      if (role.StatusSequence > 0) {
+        statusStr += `<span title="Updated ${role.UpdateDate}">⏳</span>`;
+      }
+      favoriteCell.insertAdjacentHTML('beforeend', `<div class="GM_injected status">${statusStr}</div>`);
+    }
 }
 
 function updateTable() {
     console.log("updating table");
-    document.querySelectorAll('.userscript').forEach(e => e.remove());
+    document.querySelectorAll('.GM_injected').forEach(e => e.remove());
 
     const roleRows = document.querySelectorAll('role-table-view tbody tr:not([data-type=row-details])');
 
@@ -80,6 +157,7 @@ function hookXHR() {
                     if (hit._source && hit._source.id) {
                         const id = hit._source.id;
                         sources[id] = Object.assign(sources[id] || {}, hit._source);
+                        GM_setValue('sources', sources);
                     }
                 }
                 refresh();
@@ -93,11 +171,13 @@ function hookXHR() {
                     if (v.RoleKey) {
                         const id = v.RoleKey;
                         roles[id] = Object.assign(roles[id] || {}, v);
+                        GM_setValue('roles', roles);
                         // odata endpoints have different schemas, for now just map what we need to sources
                         sources[id] = Object.assign(sources[id] || {}, {
                             id: id,
                             roleEndDate: v.EndDate
                         });
+                        GM_setValue('sources', sources);
                     }
                 }
                 refresh();
